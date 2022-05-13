3966635f4a00eb5eeda2af3f73a30ce79b5f2d60
Carl-Anders Arosenius <arosenius.carl-anders@elev.ga.ntig.se>
2022-05-13 17:31:34 +0200

    first commit

---
+++ Tryparse_Carl-Anders_TE20A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tryparse_Carl_Anders_TE20A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
