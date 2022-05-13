d70b8b1d076308276e1e73090f16d22d607d1a36
Carl-Anders Arosenius <arosenius.carl-anders@elev.ga.ntig.se>
2022-05-13 23:36:46 +0200

    varsågod

--- Tryparse_Carl-Anders_TE20A/Program.cs
+++ Tryparse_Carl-Anders_TE20A/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using Raylib_cs;
 
-namespace Tryparse_Carl_Anders_TE20A
+
+Console.WriteLine(int.MaxValue);
+
+ Console.WriteLine("Hej spelare skriv in ett tal");
+Console.WriteLine("Så att talet multipliceras med 5");
+
+string tal = Console.ReadLine();
+int resultat;
+
+bool success = int.TryParse(tal, out resultat);
+
+if (success == true)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+  Console.WriteLine(resultat * 5);
 }
+else
+{
+  Console.WriteLine("bara SIFFROR");
+}
+
+
+
+
+Console.ReadLine();
